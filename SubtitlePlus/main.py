636f1e53712c730a272286a9255082512be12a9f
jojimmy1 <66921203+jojimmy1@users.noreply.github.com>
2022-04-23 17:28:21 -0400

    [skip ci] html absolute to relative path

--- SubtitlePlus/main.py
+++ SubtitlePlus/main.py
@@ -103,9 +103,6 @@ def oneclick_done():
 
     # Split for ML dataset
     # Need to clean the output before that
-    # files = glob.glob('./serverfile/output')
-    # for f in files:
-    #     os.remove(f)
     shutil.rmtree('./serverfile/output')
     os.mkdir('./serverfile/output')
     subtitle_name = "./serverfile/oneclick.srt" #input data which is the srt text file
@@ -270,7 +267,6 @@ def create_post_done():
     
     conn.commit()
     conn.close()
-    # return "Post created" # TODO: Should redirect to profile
     url1 = f"/{hashedcode}/profile/1"
     return redirect(url1)
 
@@ -286,11 +282,6 @@ def display2(postid,hashedcode):
     name2 = name1[0][0] + ' ' + name1[0][1]
     
     post9 = (c.execute("SELECT userID,title,content from posts where post_id = ?", (postid,)).fetchall())
-    # hashid1 = (c.execute("SELECT hashcode from users where userID = ?", (post9[0][0],)).fetchall())
-    # message = f"The user is {post9[0][0]}. Title is {post9[0][1]}. Content is {post9[0][2]}. "
-    # return message # TODO: Read Post frontend
-    # conn.commit()
-    # conn.close()
     fetchall = (c.execute("SELECT vote_count,create_time from posts WHERE post_id = ?", (postid,)).fetchall())
     for element in (fetchall):
         timeget = datetime.strptime(element[1], "%Y-%m-%d %H:%M:%S.%f")
@@ -302,30 +293,6 @@ def display2(postid,hashedcode):
         half30 = 0.5*round(half60/0.5)
         count99 = element[0]
     return flask.render_template('view_post.html',postid = postid,count99 = count99,half30 = half30, content1 = post9[0][2], id1 = post9[0][0], title1 = post9[0][1], hashid1 = hashedcode, hashedcode = hashedcode, name2 = name2)
-
-# profile page (keeping the original commented just in case)
-# @app.route("/<hashedcode>/profile", methods=['GET', 'POST'])
-# def seeall(hashedcode): 
-#     db_dict = {}
-#     conn = sqlite3.connect('static/data/database.db')
-#     c = conn.cursor()
-    
-#     #get id
-#     id = (c.execute("SELECT * from users where hashcode = ?", (hashedcode,)).fetchall())
-#     id = id[0][2]
-    
-#     name1 = (c.execute("SELECT first_name,last_name,filename1 from users WHERE userID = ?", (id,)).fetchall())
-#     name2 = name1[0][0] + ' ' + name1[0][1]
-#     filename1 = name1[0][2]
-#     filename2 = "/static/../static/pic/" + filename1
-#     filename1 = filename2
-#     print(filename1)
-    
-#     fetchall = (c.execute("SELECT title,content,post_id from posts WHERE userID = ? ORDER BY create_time DESC", (id,)).fetchall())
-#     for element in (fetchall):
-#         db_dict.update({(element[0],element[2]): element[1]})
-#     print(db_dict)
-#     return flask.render_template('view2.html', data = db_dict, hashedcode = hashedcode, name2 = name2, filename1 = filename1)
 
 # Profile page using pagination
 @app.route("/<hashedcode>/profile/<pagenum>", methods=['GET', 'POST'])
@@ -388,35 +355,6 @@ def profilePagination(hashedcode, pagenum):
         i1 = i1 + 1
     
     return flask.render_template('view2.html', dict9 = dict9, data = db_dict, hashedcode = hashedcode, name2 = name2, filename1 = filename1, pagenum = pagenum)
-
-#feed page 
-# @app.route("/<hashedcode>/feed", methods=['GET', 'POST'])
-# def feedpage(hashedcode): 
-#     db_dict = {}
-#     conn = sqlite3.connect('static/data/database.db')
-#     c = conn.cursor()
-    
-#     #get id
-#     id = (c.execute("SELECT * from users where hashcode = ?", (hashedcode,)).fetchall())
-#     # list of tuple
-#     id = id[0][2]
-    
-#     name1 = (c.execute("SELECT first_name,last_name from users WHERE userID = ?", (id,)).fetchall())
-#     name2 = name1[0][0] + ' ' + name1[0][1]
-#     print(name2)
-    
-#     fetchall = (c.execute("SELECT title,content,post_id,vote_count,create_time from posts WHERE userID != ? ORDER BY create_time DESC", (id,)).fetchall())
-#     for element in (fetchall):
-#         timeget = datetime.strptime(element[4], "%Y-%m-%d %H:%M:%S.%f")
-#         now1 = datetime.now()
-#         diff1 = now1 - timeget
-#         daysec = 24 * 60 * 60 * (diff1.days)
-#         totalsec = daysec + diff1.seconds
-#         half60 = totalsec / 60 / 60
-#         half30 = 0.5*round(half60/0.5)
-#         db_dict.update({(element[0],element[2]): (element[1],element[3],half30)})
-#     print(db_dict)
-#     return flask.render_template('view3.html', data = db_dict, hashedcode = hashedcode, name2 = name2)
 
 # Feed page using pagination
 @app.route("/<hashedcode>/feed/<pagenum>", methods=['GET', 'POST'])
@@ -581,7 +519,6 @@ def closedrequest(hashedcode, pagenum):
     totalpage = round(totalpage)
     if (totalpage * 5 < count1):
         totalpage = totalpage + 1
-    print(111111111111111111111111111)
     print(count1)
     
     dict2 ={}
@@ -643,7 +580,6 @@ def vote1():
 @app.route('/delete', methods=['POST'])
 def delete1():
     """Delete a request"""
-    print('hiiiiiiiii')
     conn = sqlite3.connect('static/data/database.db')
     c = conn.cursor()
     postid = flask.request.form['postid']
@@ -657,14 +593,10 @@ def delete1():
     c.execute('DELETE FROM vote WHERE post_id=?', d1)
     c.execute('DELETE FROM posts WHERE post_id=?', d1)
     
-    
     con1 = 1
     conn.commit()
     conn.close()
     return jsonify({'count' : con1})
-
-    
-    
 
 if __name__ == '__main__':
     # Start the server
