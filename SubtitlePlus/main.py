a343d2fac171bf2749023426ff81f2b0bde2f59a
jojimmy1 <66921203+jojimmy1@users.noreply.github.com>
2022-04-27 12:23:00 -0400

    [skip ci] tweet frontend

--- SubtitlePlus/main.py
+++ SubtitlePlus/main.py
@@ -31,6 +31,17 @@ from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 # Above for video upload
 
+# Below for tweet
+from flask import Flask, render_template, request, url_for, redirect
+import oauth2 as oauth
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+from requests_oauthlib import OAuth1Session
+import tweepy
+# Above for tweet
+
 from video_split.video_split_submain import *
 
 def hash_id(id):
@@ -64,6 +75,96 @@ def merge_video_subtitle(video_filename, subtitle_filename, output_filename,scal
 # app = flask.Flask(__name__)
 app = flask.Flask(__name__, static_folder='static/')
 """Set the static folder"""
+
+# Below for tweet
+request_token_url = 'https://api.twitter.com/oauth/request_token'
+access_token_url = 'https://api.twitter.com/oauth/access_token'
+authorize_url = 'https://api.twitter.com/oauth/authorize'
+show_user_url = 'https://api.twitter.com/1.1/users/show.json'
+
+# Support keys from environment vars (Heroku).
+# app.config['APP_CONSUMER_KEY'] = "S21k0RuDj0DC7Ki3vioxjz6zM" #os.getenv('TWAUTH_APP_CONSUMER_KEY', 'API_Key_from_Twitter')
+app.config['APP_CONSUMER_KEY'] = "DR9DT9x3JWiqhLRHWNnYRbSZM" #os.getenv('TWAUTH_APP_CONSUMER_KEY', 'API_Key_from_Twitter')
+# app.config['APP_CONSUMER_SECRET'] = "I0nV5X0T3XnPFMi5fhWBSDsZkcDHKJatMvYbisj5wpZZCVrYxp" #os.getenv('TWAUTH_APP_CONSUMER_SECRET', 'API_Secret_from_Twitter')
+app.config['APP_CONSUMER_SECRET'] = "WccHxMV1comZJGhyXX4TbF7nLhUgEPp2D7dhlJnIsYIqU3sysD" #os.getenv('TWAUTH_APP_CONSUMER_SECRET', 'API_Secret_from_Twitter')
+oauth_store = {}
+payload = {"text": "initial text"}
+fields = "created_at,description"
+params = {"usernames": "TwitterDev,TwitterAPI", "user.fields": fields}
+@app.route('/callback')
+def callback():
+    # Accept the callback params, get the token and call the API to
+    # display the logged-in user's name and handle
+    oauth_token = request.args.get('oauth_token')
+    oauth_verifier = request.args.get('oauth_verifier')
+    oauth_denied = request.args.get('denied')
+
+    # if the OAuth request was denied, delete our local token
+    # and show an error message
+    if oauth_denied:
+        if oauth_denied in oauth_store:
+            del oauth_store[oauth_denied]
+        return render_template('error.html', error_message="the OAuth request was denied by this user")
+
+    if not oauth_token or not oauth_verifier:
+        return render_template('error.html', error_message="callback param(s) missing")
+
+    # unless oauth_token is still stored locally, return error
+    if oauth_token not in oauth_store:
+        return render_template('error.html', error_message="oauth_token not found locally")
+
+    oauth_token_secret = oauth_store[oauth_token]
+
+    # if we got this far, we have both callback params and we have
+    # found this token locally
+
+    consumer = oauth.Consumer(
+        app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'])
+    token = oauth.Token(oauth_token, oauth_token_secret)
+    token.set_verifier(oauth_verifier)
+    client = oauth.Client(consumer, token)
+
+    resp, content = client.request(access_token_url, "POST")
+    access_token = dict(urllib.parse.parse_qsl(content))
+
+    screen_name = access_token[b'screen_name'].decode('utf-8')
+    user_id = access_token[b'user_id'].decode('utf-8')
+
+    # These are the tokens you would store long term, someplace safe
+    real_oauth_token = access_token[b'oauth_token'].decode('utf-8')
+    real_oauth_token_secret = access_token[b'oauth_token_secret'].decode(
+        'utf-8')
+
+    # Call api.twitter.com/1.1/users/show.json?user_id={user_id}
+    real_token = oauth.Token(real_oauth_token, real_oauth_token_secret)
+    real_client = oauth.Client(consumer, real_token)
+
+
+    oauth_test = OAuth1Session(
+        app.config['APP_CONSUMER_KEY'],
+        client_secret=app.config['APP_CONSUMER_SECRET'],
+        resource_owner_key=real_oauth_token,
+        resource_owner_secret=real_oauth_token_secret,
+    )
+    real_resp = oauth_test.post(
+    "https://api.twitter.com/2/tweets",
+    json=payload,
+    )
+    
+    #real_resp, real_content = real_client.request(
+    #    show_user_url + '?user_id=' + user_id, "GET")
+    if real_resp.status_code != 201:
+        raise Exception(
+            "Request returned an error: {} {}".format(real_resp.status_code, real_resp.text)
+        )
+
+    print("Response code: {}".format(real_resp.status_code))
+    # don't keep this token and secret in memory any longer
+    del oauth_store[oauth_token]
+
+    return flask.render_template("tweeted.html")
+# Above for tweet
+###################################################
 
 # Below for video upload
 CLIENT_SECRETS_FILE = "client_secrets.json"
@@ -137,6 +238,36 @@ def test_api_request():
     #              credentials in a persistent database instead.
     flask.session['credentials'] = credentials_to_dict(credentials)
     return flask.render_template("uploaded.html")
+
+@app.route('/tweet_done', methods=['GET','POST'])
+def tweet_done():
+    global payload
+    content = flask.request.form['descr']
+    payload["text"] = content
+
+    # note that the external callback URL must be added to the whitelist on
+    # the developer.twitter.com portal, inside the app settings
+    app_callback_url = url_for('callback', _external=True)
+
+    # Generate the OAuth request tokens, then display them
+    consumer = oauth.Consumer(
+        app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'])
+    client = oauth.Client(consumer)
+    resp, content = client.request(request_token_url, "POST", body=urllib.parse.urlencode({
+                                   "oauth_callback": app_callback_url}))
+
+    if resp['status'] != '200':
+        error_message = 'Invalid response, status {status}, {message}'.format(
+            status=resp['status'], message=content.decode('utf-8'))
+        return error_message
+
+    request_token = dict(urllib.parse.parse_qsl(content))
+    oauth_token = request_token[b'oauth_token'].decode('utf-8')
+    oauth_token_secret = request_token[b'oauth_token_secret'].decode('utf-8')
+
+    oauth_store[oauth_token] = oauth_token_secret
+    
+    return redirect(authorize_url +f"?oauth_token={oauth_token}")
 
 def resumable_upload(insert_request):
     """This method implements an exponential backoff strategy to resume a failed upload."""
@@ -314,6 +445,11 @@ def mldata():
 def merge_data():
     """Render the export front end"""
     return flask.render_template("export.html")
+
+@app.route("/tweet", methods=['GET','POST'])
+def twitter():
+    """Send a tweet"""
+    return flask.render_template("upload_twitter.html")
 
 @app.route("/upload", methods=['GET','POST'])
 def uploadvideo():
